cd894c8db90a4c68b4ff8ed61a54f86a65478e60
Artem <xamber-zenit@mail.ru>
2016-10-19 14:20:10 +0300

    :lemon: Hacking..

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import {BrowserModule}  from '@angular/platform-browser';
 import {FormsModule}    from '@angular/forms';
 import {AppComponent}   from './app';
 import {HelloComponent} from './hello';
+import {DietComponent} from './diet';
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HelloComponent],
+    declarations: [AppComponent, HelloComponent, DietComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
