d48f467c65fec2b4d170c54cbb52081c6636fbd5
Artem <xamber-zenit@mail.ru>
2016-10-19 17:01:37 +0300

    :lemon: Hacking!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import {NgModule}       from '@angular/core';
 import {BrowserModule}  from '@angular/platform-browser';
 import {FormsModule}    from '@angular/forms';
+import {RouterModule}   from '@angular/router';
 import {AppComponent}   from './app';
 import {HelloComponent} from './hello';
-import {DietComponent} from './diet';
+import {ProductsComponent} from './products';
+
 
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HelloComponent, DietComponent],
+    imports: [BrowserModule, FormsModule, RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/hello',
+        pathMatch: 'full'
+      },
+      {
+        path: 'hello',
+        component: HelloComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+    ])
+  ],
+    declarations: [AppComponent, HelloComponent, ProductsComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
